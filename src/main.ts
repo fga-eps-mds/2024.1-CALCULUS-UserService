9af7021c014e21a72f31f3290712ff1d657e130c
DaviMatheus <matdavimat@gmail.com>
2024-08-25 19:36:49 -0300

    fixing_Lint

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { config } from 'dotenv';
 
 const configService = new ConfigService();
 const logger = new Logger('Main');
@@ -10,7 +9,7 @@ const logger = new Logger('Main');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors()
+  app.enableCors();
   await app.listen(configService.get('PORT'));
   logger.log(`Application listening on port ${configService.get('PORT')}`);
 }
